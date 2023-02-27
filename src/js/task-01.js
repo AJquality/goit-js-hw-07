a7dfa3e2e4303c63f43597937d592779d9a442ae
Adrian Jurczuk <jurczuk.adrian@gmail.com>
2023-02-27 08:31:50 +0100

    zrobione do zadania 3

---
+++ src/js/task-01.js
@@ -0,0 +1,7 @@
+const category = document.querySelectorAll(".item");
+console.log("Number of categories:" + " " + category.length);
+
+category.forEach((name) => {
+  console.log(`Category: ${name.firstElementChild.textContent}`);
+  console.log(`Elements: ${name.lastElementChild.children.length}`);
+});
