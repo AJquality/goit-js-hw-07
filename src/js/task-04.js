68eb40e79ca3aa18ccbbc18296c39528c4444f63
Adrian Jurczuk <jurczuk.adrian@gmail.com>
2023-02-28 21:59:32 +0100

    zadania 1-4 modul 7

---
+++ src/js/task-04.js
@@ -0,0 +1,28 @@
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const amount = document.querySelector("input");
+const boxes = document.querySelector("#boxes");
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function createBoxes(value) {
+  let size = 30;
+  for (let i = 1; i <= value; size += 10) {
+    boxes.insertAdjacentHTML(
+      "afterbegin",
+      `<div style="width:${size}px;height:${size}px;backgroundColor: ${getRandomHexColor()}"></div>`
+    );
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+createButton.addEventListener("click", () => createBoxes(amount.value));
+
+destroyButton.addEventListener("click", () => destroyBoxes());
